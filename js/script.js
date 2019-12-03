5847d3532593beb4e651de0ce6c069817f20fc05
Guilherme Araujo <40188385+guilhermearaujo14@users.noreply.github.com>
2019-12-03 19:56:22 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 document.getElementById('formulario').addEventListener('submit', function()
 {
     let nome = this.querySelector('input[name=nome]'), nome=nome.value;
-    let email = this.querySelector('input[name=cpf]'), email=email.value;
     let telefone = this.querySelector('input[name=fone]'), telefone=fone.value;
-
     let campeao = this.querySelector('input[name=campeao]'), campeao=campeao.value;
+    
 });
